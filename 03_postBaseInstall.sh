51b98fa55192373f649b81cc1607cfd98f2756fa
quantumsnippet <darshan.haragi9@gmail.com>
2021-07-05 19:46:06 +0530

    Added enabling parallel downloads, Pacman hooks for cleaning package cache and a pacman hook for dealing with .pacnew files, added a limit for journal files and other minor improvements

--- 03_postBaseInstall.sh
+++ 03_postBaseInstall.sh
@@ -1,4 +1,4 @@
-echo """"""ADDING USER AND GIVING SUDO PRIVILEGES""""""
+echo "ADDING USER AND GIVING SUDO PRIVILEGES"
 useradd darshan
 passwd darshan
 usermod -aG wheel darshan
@@ -10,54 +10,41 @@ groups
 sleep 5
 sudo whoami
 sleep 5
-echo "-------------------------------------------------"
+echo "################################################################"
 
-
-echo """"""INCLUDING MULTILIB REPOSITORY AND ENABLING PARALLEL DOWNLOADS""""""
+echo "INCLUDING MULTILIB REPOSITORY AND ENABLING PARALLEL DOWNLOADS"
 sudo sed '/ParallelDownloads = 5/s/^#//' -i /etc/pacman.conf
-echo """"""Uncomment the [multilib] repository line, its mirrorlist include line and save it""""""
+echo "Uncomment the [multilib] repository line, its mirrorlist include line and save it"
 sleep 10
 sudo vim /etc/pacman.conf
-echo "-------------------------------------------------"
+echo "################################################################"
 
-
-echo """"""UPDATING THE REPOSITORIES""""""
+echo "UPDATING THE REPOSITORIES"
 sudo pacman -Syyu --noconfirm
-echo "-------------------------------------------------"
+echo "################################################################"
 
-
-echo """"""INSTALLATION OF XORG, DESKTOP ENVIRONMENT AND LOGIN DISPLAY MANAGER""""""
+echo "INSTALLATION OF XORG, DESKTOP ENVIRONMENT AND LOGIN DISPLAY MANAGER"
 sudo pacman -S xorg --noconfirm
 sudo pacman -S mate mate-extra --noconfirm
 sudo pacman -S lxdm --noconfirm
 sudo systemctl enable lxdm.service
-echo "-------------------------------------------------"
+echo "################################################################"
 
-
-echo """"""WIFI CONNECTION""""""
+echo "WIFI CONNECTION"
 sudo pacman -S broadcom-wl-dkms network-manager-applet --noconfirm
 sudo rmmod b43 b43legacy bcm43xx bcma brcm80211 brcmfmac brcmsmac ssb wl
 sudo modprobe wl
-echo "-------------------------------------------------"
+echo "################################################################"
 
-
-echo """"""INSTALLING MY NECESSARY PACKAGES""""""
+echo "INSTALLING MY NECESSARY PACKAGES"
 sudo pacman -S pacman-contrib p7zip unrar zip unzip gvfs gvfs-mtp youtube-dl vlc libreoffice-fresh firefox htop neofetch gdb geany bleachbit ranger git curl wget gvim vim-airline vim-airline-themes vim-syntastic qbittorrent xfce4-terminal keepassxc --noconfirm
-echo "-------------------------------------------------"
- 
+echo "################################################################"
 
-echo """"""INSTALLING PACKAGES FOR PLAYING SOUND""""""
+echo "INSTALLING PACKAGES FOR PLAYING SOUND"
 sudo pacman -S pulseaudio pulseaudio-alsa pavucontrol alsa-utils alsa-plugins alsa-lib alsa-firmware gstreamer volumeicon --noconfirm
-echo "-------------------------------------------------"
+echo "################################################################"
 
-
-echo """"""MAKING USE OF ALL CORES FOR COMPILING""""""
-echo """"""Search for "MAKEFLAGS" and replace the "-j2" with "-j$(nproc)"
-sudo vim /etc/makepkg.conf
-echo "-------------------------------------------------"
-
-
-echo """"""CLEANING PACMAN CACHE HOOK""""""
+echo "CLEANING PACMAN CACHE HOOK"
 sudo mkdir /etc/pacman.d/hooks
 sudo touch /etc/pacman.d/hooks/clean_package_cache.hook
 cat > /etc/pacman.d/hooks/clean_package_cache.hook <<EOF
@@ -73,13 +60,11 @@ Description = CLEANING PACMAN CACHE...
 When = PostTransaction
 Exec = /usr/bin/paccache -r
 EOF
-echo "-------------------------------------------------"
+echo "################################################################"
 
-
-echo """"""CLEANING PACMAN CACHE HOOK""""""
+echo "CLEANING PACMAN CACHE HOOK"
 sudo touch /etc/pacman.d/hooks/pacdiff.hook
 cat > /etc/pacman.d/hooks/pacdiff.hook <<EOF
-
 [Trigger]
 Operation = Install
 Operation = Upgrade
@@ -92,15 +77,13 @@ Description = CHECKING FOR PACNEW FILES...
 When = PostTransaction
 Exec = /usr/bin/pacdiff
 EOF
-echo "-------------------------------------------------"
+echo "################################################################"
 
-
-echo """"""SETTING JOURNAL SIZE LIMIT""""""
+echo "SETTING JOURNAL SIZE LIMIT"
 sudo sed '/SystemMaxUse/s/^#//' -i /etc/systemd/journald.conf
 sudo systemctl restart systemd-journald.service
-echo "-------------------------------------------------"
+echo "################################################################"
 
-
-echo """"""REBOOT""""""
+echo "REBOOT"
 sleep 5
 reboot
